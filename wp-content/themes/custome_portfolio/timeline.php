69195066f6b875ce45bb0173d4881a07d95370d6
jbs321 <jbs321.work@gmail.com>
2016-01-06 22:53:46 +0200

    more fix ups

--- wp-content/themes/custome_portfolio/timeline.php
+++ wp-content/themes/custome_portfolio/timeline.php
@@ -13,18 +13,15 @@
             $eDate = date("d-m-Y", $custom_fields['wpcf-end_date'][0]);
             $content = $custom_fields['wpcf-content'][0];
         ?>
-
-                <li>
-                    <time class="cl_time" datetime="<?=$sDate?>"><span><?=$sDate?></span></time>
-<!--                    <div class="cl_icon cbp_tmicon-phone"></div>-->
-                    <i class="start_icon fa fa-flag"></i>
-                    <div class="cl_label">
-                        <h1><?=$title?></h1>
-                        <h2><?=$sub_title?></h2>
-                        <p><?=$content?></p>
-                    </div>
-                </li>
-
+            <li>
+                <time class="cl_time" datetime="<?=$sDate?>"><span><?=$sDate?></span></time>
+                <i class="start_icon fa fa-flag"></i>
+                <div class="shake cl_label">
+                    <h1><?=$title?></h1>
+                    <h2><?=$sub_title?></h2>
+                    <p><?=$content?></p>
+                </div>
+            </li>
         <?php endforeach; ?>
         </ul>
     </div>
