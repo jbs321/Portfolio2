3d01d1ee10ef2fe18f4e351aeab70200ddb48392
jbs321 <jbs321.work@gmail.com>
2016-01-07 00:14:00 +0200

    add email send test

--- test-mail.php
+++ test-mail.php
@@ -1,2 +1,3 @@
 <?php
-print (mail("jbs321@gmail.com", "{$_REQUEST['name']} - New Contact - Portfolio",$_REQUEST['email'] .' /n '. $_REQUEST['msg'])) ? "Email successfully sent" : print "An error occured" ;
+mail("jbs321@gmail.com", "{$_REQUEST['name']} - New Contact - Portfolio",$_REQUEST['email'] .' /n '. $_REQUEST['msg']);
+?>
