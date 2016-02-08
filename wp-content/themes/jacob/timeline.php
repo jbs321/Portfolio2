d43857a77e13ac6a362d4703fc522464d801c3e7
jbs321 <jbs321.work@gmail.com>
2016-02-08 16:17:01 +0200

    3ed try

--- wp-content/themes/jacob/timeline.php
+++ wp-content/themes/jacob/timeline.php
@@ -1,27 +1,50 @@
 <div class="section container-fluid timeline" id="timeline">
-    <section class="cd-horizontal-timeline">
+
+    <h1 class="section-header">MY ENTIRE CAREER</h1>
+
+    <section class="col-xs-4 education">
+        <h2>Education</h2>
+
+        <div class="col-xs-12 box">
+            <div class="range">2004 - 2005</div>
+            <div class="description">
+                <div class="bold">Univercity Name</div>
+                <span class="text">sfdas asd asd asd asd sadasf asfas</span>
+            </div>
+        </div>
+
+        <div class="col-xs-12 box">
+            <div class="range">2004 - 2005</div>
+            <div class="description">
+                <div class="bold">Univercity Name</div>
+                <span class="text">sfdas asd asd asd asd sadasf asfas</span>
+            </div>
+        </div>
+    </section>
+
+    <section class="cd-horizontal-timeline col-xs-8">
         <div class="timeline">
             <div class="events-wrapper">
                 <div class="events">
                     <ol>
-<!--                        <li><a href="#0" data-date="16/01/2014" class="selected">16 Jan</a></li>-->
-<!--                        <li><a href="#0" data-date="28/02/2014">28 Feb</a></li>-->
-<!--                        <li><a href="#0" data-date="20/04/2014">20 Mar</a></li>-->
-<!--                        <li><a href="#0" data-date="20/05/2014">20 May</a></li>-->
+                        <li><a href="#0" data-date="16/01/2014" class="selected">16 Jan</a></li>
+                        <li><a href="#0" data-date="28/02/2014">28 Feb</a></li>
+                        <li><a href="#0" data-date="20/04/2014">20 Mar</a></li>
+                        <li><a href="#0" data-date="20/05/2014">20 May</a></li>
 
 
-                    <?php $first = 0;
-                    foreach (get_posts(['post_type' => 'timeline']) as $post):
-                        $custom_fields = get_post_custom($post->ID);
-                        $title = $custom_fields['wpcf-title'][0];
-                        $sub_title = $custom_fields['wpcf-sub_title'][0];
-                        $sDate = date("d/m/Y", $custom_fields['wpcf-start_date'][0]);
-                        $eDate = date("dd/mm/YYYY", $custom_fields['wpcf-end_date'][0]);
-                        $content = $custom_fields['wpcf-content'][0];
-                        ?>
-                        <li><a href="#0" data-date="<?=$sDate?>" <?php if($first == 0): ?> class="selected" <?php endif; ?> ><?=$sDate?></a></li>
-                        <?php $first++; ?>
-                    <?php endforeach; ?>
+<!--                    --><?php //$first = 0;
+//                    foreach (get_posts(['post_type' => 'timeline']) as $post):
+//                        $custom_fields = get_post_custom($post->ID);
+//                        $title = $custom_fields['wpcf-title'][0];
+//                        $sub_title = $custom_fields['wpcf-sub_title'][0];
+//                        $sDate = date("d/m/Y", $custom_fields['wpcf-start_date'][0]);
+//                        $eDate = date("dd/mm/YYYY", $custom_fields['wpcf-end_date'][0]);
+//                        $content = $custom_fields['wpcf-content'][0];
+//                        ?>
+<!--                        <li><a href="#0" data-date="--><?//=$sDate?><!--" --><?php //if($first == 0): ?><!-- class="selected" --><?php //endif; ?><!-- >--><?//=$sDate?><!--</a></li>-->
+<!--                        --><?php //$first++; ?>
+<!--                    --><?php //endforeach; ?>
 
                     </ol>
 
@@ -37,56 +60,56 @@
 
         <div class="events-content">
             <ol>
-            <?php $first = 0;
-            foreach (get_posts(['post_type' => 'timeline']) as $post):
-                $custom_fields = get_post_custom($post->ID);
-                $title = $custom_fields['wpcf-title'][0];
-                $sub_title = $custom_fields['wpcf-sub_title'][0];
-                $sDate = date("d/m/Y", $custom_fields['wpcf-start_date'][0]);
-                $eDate = date("dd/mm/YYYY", $custom_fields['wpcf-end_date'][0]);
-                $content = $custom_fields['wpcf-content'][0];
-                ?>
-                <li <?php if($first == 0): ?> class="selected" <?php endif; ?> data-date="<?=$sDate ?>">
-                    <h2><?=$title?></h2>
-                    <em>January 16th, 2014</em>
-                    <p>
-                        test text
-                    </p>
-                </li>
-                <?php $first++;?>
-            <?php endforeach; ?>
-
-<!--                <li class="selected" data-date="16/01/2014">-->
-<!--                    <h2>Horizontal Timeline</h2>-->
+<!--            --><?php //$first = 0;
+//            foreach (get_posts(['post_type' => 'timeline']) as $post):
+//                $custom_fields = get_post_custom($post->ID);
+//                $title = $custom_fields['wpcf-title'][0];
+//                $sub_title = $custom_fields['wpcf-sub_title'][0];
+//                $sDate = date("d/m/Y", $custom_fields['wpcf-start_date'][0]);
+//                $eDate = date("dd/mm/YYYY", $custom_fields['wpcf-end_date'][0]);
+//                $content = $custom_fields['wpcf-content'][0];
+//                ?>
+<!--                <li --><?php //if($first == 0): ?><!-- class="selected" --><?php //endif; ?><!-- data-date="--><?//=$sDate ?><!--">-->
+<!--                    <h2>--><?//=$title?><!--</h2>-->
 <!--                    <em>January 16th, 2014</em>-->
 <!--                    <p>-->
 <!--                        test text-->
 <!--                    </p>-->
 <!--                </li>-->
-<!---->
-<!--                <li data-date="28/02/2014">-->
-<!--                    <h2>Event title here</h2>-->
-<!--                    <em>February 28th, 2014</em>-->
-<!--                    <p>-->
-<!--                        test text-->
-<!--                    </p>-->
-<!--                </li>-->
-<!---->
-<!--                <li data-date="20/04/2014">-->
-<!--                    <h2>Event title here</h2>-->
-<!--                    <em>March 20th, 2014</em>-->
-<!--                    <p>-->
-<!--                        test text-->
-<!--                    </p>-->
-<!--                </li>-->
-<!---->
-<!--                <li data-date="20/05/2014">-->
-<!--                    <h2>Event title here</h2>-->
-<!--                    <em>May 20th, 2014</em>-->
-<!--                    <p>-->
-<!--                        test text-->
-<!--                    </p>-->
-<!--                </li>-->
+<!--                --><?php //$first++;?>
+<!--            --><?php //endforeach; ?>
+
+                <li class="selected" data-date="16/01/2014">
+                    <h2>Horizontal Timeline</h2>
+                    <em>January 16th, 2014</em>
+                    <p>
+                        test text
+                    </p>
+                </li>
+
+                <li data-date="28/02/2014">
+                    <h2>Event title here</h2>
+                    <em>February 28th, 2014</em>
+                    <p>
+                        test text
+                    </p>
+                </li>
+
+                <li data-date="20/04/2014">
+                    <h2>Event title here</h2>
+                    <em>March 20th, 2014</em>
+                    <p>
+                        test text
+                    </p>
+                </li>
+
+                <li data-date="20/05/2014">
+                    <h2>Event title here</h2>
+                    <em>May 20th, 2014</em>
+                    <p>
+                        test text
+                    </p>
+                </li>
             </ol>
         </div> <!-- .events-content -->
     </section>
